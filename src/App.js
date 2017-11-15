31e389ee10a4b4c7aaf60471d376384db6445761
Ryan Yosua <hi@ryanyosua.me>
2017-11-14 21:09:44 -0500

    Added react-bootstrap and a nav bar.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 import './App.css'
 import NewsFeedContainer from './containers/NewsFeedContainer'
+
+const signedIn = false
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1>Fragmented</h1>
-                </header>
+                <Navbar inverse>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Fragmented</a>
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem href="#">About</NavItem>
+                        <NavItem href="#">Blog</NavItem>
+                        <NavDropdown title="Account" id="basic-nav-dropdown">
+                            <MenuItem>Sign up</MenuItem>
+                            <MenuItem>Log in</MenuItem>
+                            <MenuItem> Submissions</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar>
                 <NewsFeedContainer />
             </div>
         )
