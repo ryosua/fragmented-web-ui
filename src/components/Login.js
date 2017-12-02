486aee7a5bc5142b6d6c99b8025abb90b45ca793
Ryan Yosua <hi@ryanyosua.me>
2017-12-02 17:04:37 -0500

    Add a login page.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,54 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { ControlLabel, FormControl, Button } from 'react-bootstrap'
+import defaultTo from 'lodash/defaultTo'
 
-const Login = props => <p>Login (Coming soon)</p>
+const onchangeHandler = (handler, fieldName) => e => handler(e, fieldName)
+
+const fieldNames = {
+    email: 'emailAddressValue',
+    username: 'usernameValue',
+    password: 'passwordValue',
+    confirmPassword: 'confirmPasswordValue'
+}
+
+const Login = props => {
+    const userMesssage = defaultTo(props.userMessage, '')
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <br />
+            <ControlLabel>Email Address</ControlLabel>
+            <FormControl
+                type="email"
+                placeholder="Enter email"
+                value={props[fieldNames.email]}
+                onChange={onchangeHandler(props.handleTextFieldChange, fieldNames.email)}
+            />
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+                type="password"
+                placeholder="Enter a password"
+                value={props[fieldNames.password]}
+                onChange={onchangeHandler(props.handleTextFieldChange, fieldNames.password)}
+            />
+            <br />
+            <Button type="submit" onClick={props.handleOnLoginPress}>
+                Login
+            </Button>
+            <br />
+            <p>{userMesssage}</p>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    emailAddressValue: PropTypes.string.isRequired,
+    passwordValue: PropTypes.string.isRequired,
+    handleTextFieldChange: PropTypes.func.isRequired,
+    handleOnLoginPress: PropTypes.func.isRequired,
+    userMessage: PropTypes.string
+}
 
 export default Login
