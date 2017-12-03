22896d5cedd16c55e2411180cfa9c6ddebd2a4cd
Ryan Yosua <hi@ryanyosua.me>
2017-12-03 15:20:34 -0500

    Add token to requests.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,23 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink, concat } from 'apollo-link'
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/Fragmented' })
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+    operation.setContext({
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('token')}` || null
+        }
+    })
+
+    return forward(operation)
+})
+
 const client = new ApolloClient({
-    link: createHttpLink({ uri: 'https://api.graph.cool/simple/v1/Fragmented' }),
+    link: concat(authMiddleware, httpLink),
     cache: new InMemoryCache()
 })
 
