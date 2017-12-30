79b10c7ad803506e6054bd279a288035ff3bf18f
Ryan Yosua <hi@ryanyosua.me>
2017-12-30 14:42:03 -0500

    Added comments to the GetNewsFeed query.

--- src/graphql/queries/GetNewsFeed.js
+++ src/graphql/queries/GetNewsFeed.js
@@ -3,8 +3,15 @@ import gql from 'graphql-tag'
 const GetNewsFeed = gql`
     query GetNewsFeedQuery($after: String) {
         allNewsItems(orderBy: creationTime_DESC, first: 12, after: $after) {
+            __typename
             id
             title
+            comments {
+                __typename
+                id
+                creationTime
+                text
+            }
         }
     }
 `
