81d77ea968bc26fe2c21105a4e7e1075dda1cf70
Ryan Yosua <hi@ryanyosua.me>
2018-01-07 16:19:54 -0500

    Load 7 news items at a time.

--- src/graphql/queries/GetNewsFeed.js
+++ src/graphql/queries/GetNewsFeed.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 const GetNewsFeed = gql`
     query GetNewsFeedQuery($after: String) {
-        allNewsItems(orderBy: creationTime_DESC, first: 12, after: $after) {
+        allNewsItems(orderBy: creationTime_DESC, first: 7, after: $after) {
             __typename
             id
             type
