7156832ce01549bc6b97a2f1fd3b50cd6993e7fa
Ryan Yosua <hi@ryanyosua.me>
2018-02-07 19:46:43 -0500

    Prompt for $1 tip when the button is clicked.

--- src/components/NewsFeedItemDetail.js
+++ src/components/NewsFeedItemDetail.js
@@ -4,6 +4,27 @@ import { Link } from 'react-router-dom'
 import text from 'util/text'
 import tippable from 'util/tippable'
 
+const eth = window.web3.eth
+const amount = 1292607577265618
+
+const tip = user =>
+    eth.sendTransaction(
+        {
+            from: eth.coinbase,
+            to: user.publicAddress,
+            value: amount
+        },
+        (error, result) => {
+            if (!error) {
+                console.log('Transaction successul!')
+                console.log(result)
+            } else {
+                console.log('Error sending transaction.')
+                console.log(error)
+            }
+        }
+    )
+
 const NewsFeedItemDetail = (props, context) => {
     const web3Context = context.web3
     const { user, pathname, id, comments } = props
@@ -15,7 +36,7 @@ const NewsFeedItemDetail = (props, context) => {
         <div>
             <p>
                 {`${text.NewsItems.usernamePrefix} ${user.username}  `}
-                {tippable(web3Context, user) && <a>(tip)</a>}
+                {tippable(web3Context, user) && <a onClick={() => tip(user)}>(tip)</a>}
             </p>
             <Link to={{ pathname: pathname, search: `?id=${id}` }}>{`${numberOfComments} ${text.Comments.comments(
                 numberOfComments
