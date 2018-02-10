a6c36da5d8c1c9b53b38ed465e9d21d2f7b1de6f
Ryan Yosua <hi@ryanyosua.me>
2018-02-10 12:45:24 -0500

    Preview posts in real time.

--- src/components/CreateTextNewsItemForm.js
+++ src/components/CreateTextNewsItemForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ReactMarkdown from 'react-markdown'
 import ErrorComponent from 'util/ErrorHandler'
 import FormField from 'components/FormField'
 import ActionButton from 'components/ActionButton'
@@ -29,7 +30,10 @@ const CreateTextNewsItemForm = props => {
                 value={props[fieldNames.text]}
                 onChange={onchangeHandler(props.handleTextFieldChange, fieldNames.text)}
             />
+            <a href="http://commonmark.org/">Markdown supported</a>
             <br />
+            <h4>Preview:</h4>
+            <ReactMarkdown source={props.textValue} />
             <ActionButton label="Submit" onClick={props.handleSubmitPress} />
             <br />
             {props.hasError && <ErrorComponent error={props.error} errorMessage={props.errorMessage} />}
