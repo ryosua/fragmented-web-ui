c20019250c8c5d7adee850d46e916ceff8ef9cb6
Ryan Yosua <hi@ryanyosua.me>
2018-02-10 16:35:01 -0500

    Disable the post comment button if the comment text is null.

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -2,15 +2,16 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Button } from 'react-bootstrap'
 
-const ActionButton = props => (
-    <Button type="submit" onClick={props.onClick}>
-        {props.label}
+const ActionButton = ({ label, onClick, disabled }) => (
+    <Button type="submit" onClick={onClick} disabled={disabled}>
+        {label}
     </Button>
 )
 
 ActionButton.propTypes = {
     label: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool
 }
 
 export default ActionButton
--- src/components/PostCommentForm.js
+++ src/components/PostCommentForm.js
@@ -4,17 +4,17 @@ import FormField from 'components/FormField'
 import ActionButton from 'components/ActionButton'
 import text from 'util/text'
 
-const PostCommentForm = props => (
+const PostCommentForm = ({ comment, onChange, onSubmit }) => (
     <div>
         <FormField
             label={''}
             type="textarea"
             placeholder={text.Comments.formPlaceholder}
-            value={props.comment}
-            onChange={props.onChange}
+            value={comment}
+            onChange={onChange}
         />
         <br />
-        <ActionButton label={text.Comments.formButtonLabel} onClick={props.onSubmit} />
+        <ActionButton label={text.Comments.formButtonLabel} onClick={onSubmit} disabled={!comment} />
     </div>
 )
 
