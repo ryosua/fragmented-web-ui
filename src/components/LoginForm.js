f36f40819413d58d34ab354ee9d49364dae7d9b8
Ryan Yosua <hi@ryanyosua.me>
2018-02-24 08:56:50 -0500

    Remove unused imports.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ErrorComponent from 'util/ErrorHandler'
-import { extend } from 'styled-components'
 import FormField from 'components/FormField'
 import ActionButton from 'components/ActionButton'
 import text from 'util/text'
--- src/styles/FlexColumn.js
+++ src/styles/FlexColumn.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
 
 const FlexColumn = styled.div`
     display: flex;
