fb5f389c2bf3594de7449dc157fcac0569056ed0
Ryan Yosua <hi@ryanyosua.me>
2018-02-24 10:20:10 -0500

    Don't show authors as tippable if Metmask is locked.

--- src/util/tippable.js
+++ src/util/tippable.js
@@ -1,5 +1,5 @@
 const tippable = (web3Context, user) => {
-    return web3Context.networkId === '1' && user.publicAddress
+    return web3Context.networkId === '1' && web3Context.selectedAccount && user.publicAddress
 }
 
 export default tippable
