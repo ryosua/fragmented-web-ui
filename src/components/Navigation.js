ebc4bbcd3dfc6e662d9815b884b5bef49b731640
Ryan Yosua <hi@ryanyosua.me>
2018-02-24 08:36:33 -0500

    Made navbar responsive.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,34 +10,37 @@ const Navigation = props => (
             <Navbar.Brand>
                 <a href="/newest">{text.app.name}</a>
             </Navbar.Brand>
+            <Navbar.Toggle />
         </Navbar.Header>
-        <Nav>
-            {props.isLoggedIn && <NavItem href="/create-post">{text.Posting.navigationLinkText}</NavItem>}
-            <NavItem href="http://www.ryanyosua.me/decentralized-social-network/">{text.app.aboutLinkText}</NavItem>
-            <NavItem href="http://www.ryanyosua.me/">{text.app.blogLinkText}</NavItem>
-            <NavDropdown title="Account" id="basic-nav-dropdown">
-                {!props.isLoggedIn && (
-                    <LinkContainer to="/signup">
-                        <MenuItem>{text.Signup.title}</MenuItem>
-                    </LinkContainer>
-                )}
-                {!props.isLoggedIn && (
-                    <LinkContainer to="/login">
-                        <MenuItem>{text.Login.title}</MenuItem>
-                    </LinkContainer>
-                )}
-                {props.isLoggedIn && (
-                    <LinkContainer to="/submissions">
-                        <MenuItem>{text.Submissions.title}</MenuItem>
-                    </LinkContainer>
-                )}
-                {props.isLoggedIn && (
-                    <LinkContainer to="/logout">
-                        <MenuItem>{text.app.logout}</MenuItem>
-                    </LinkContainer>
-                )}
-            </NavDropdown>
-        </Nav>
+        <Navbar.Collapse>
+            <Nav>
+                {props.isLoggedIn && <NavItem href="/create-post">{text.Posting.navigationLinkText}</NavItem>}
+                <NavItem href="http://www.ryanyosua.me/decentralized-social-network/">{text.app.aboutLinkText}</NavItem>
+                <NavItem href="http://www.ryanyosua.me/">{text.app.blogLinkText}</NavItem>
+                <NavDropdown title="Account" id="basic-nav-dropdown">
+                    {!props.isLoggedIn && (
+                        <LinkContainer to="/signup">
+                            <MenuItem>{text.Signup.title}</MenuItem>
+                        </LinkContainer>
+                    )}
+                    {!props.isLoggedIn && (
+                        <LinkContainer to="/login">
+                            <MenuItem>{text.Login.title}</MenuItem>
+                        </LinkContainer>
+                    )}
+                    {props.isLoggedIn && (
+                        <LinkContainer to="/submissions">
+                            <MenuItem>{text.Submissions.title}</MenuItem>
+                        </LinkContainer>
+                    )}
+                    {props.isLoggedIn && (
+                        <LinkContainer to="/logout">
+                            <MenuItem>{text.app.logout}</MenuItem>
+                        </LinkContainer>
+                    )}
+                </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
     </Navbar>
 )
 
