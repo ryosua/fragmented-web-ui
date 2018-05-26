100444265fadabd31375719983f5843c6a807612
Ryan Yosua <hi@ryanyosua.me>
2018-05-26 19:24:35 -0400

    Create an AuthContext

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'App.css'
-import isEmpty from 'lodash/isEmpty'
-import get from 'lodash/get'
+import AuthContext from 'contexts/AuthContext'
 import { graphql } from 'react-apollo'
 import Home from 'components/Home'
 import NewsFeedContainer from 'containers/NewsFeedContainer'
@@ -18,19 +17,12 @@ import UpdateUserPublicAddress from 'graphql/mutations/UpdateUserPublicAddress'
 import getPublicAddressFromContext from 'util/getPublicAddressFromContext'
 import getConversionRate from 'util/getConversionRate'
 
-const storageKeys = {
-    USER: 'user',
-    TOKEN: 'token'
-}
-
 class App extends Component {
-    constructor(props) {
-        super(props)
-        const token = localStorage.getItem(storageKeys.TOKEN)
-        const storedUserJSON = localStorage.getItem(storageKeys.USER)
-        const storedUser = storedUserJSON && storedUserJSON !== 'undefined' && JSON.parse(storedUserJSON)
-        const isLoggedIn = !isEmpty(token) && !isEmpty(storedUser)
-        this.state = { isLoggedIn: isLoggedIn, storedUser: isLoggedIn ? storedUser : undefined }
+    state = {}
+
+    static propTypes = {
+        onLogin: PropTypes.func.isRequired,
+        onLogout: PropTypes.func.isRequired
     }
 
     componentDidMount() {
@@ -53,56 +45,43 @@ class App extends Component {
             })
     }
 
-    onLogin = data => {
-        const user = data.signinUser.user
-        localStorage.setItem(storageKeys.TOKEN, data.signinUser.token)
-        localStorage.setItem(storageKeys.USER, JSON.stringify(user))
-        this.setState({ isLoggedIn: true, storedUser: user })
-    }
-
-    onLogout = () => {
-        localStorage.removeItem(storageKeys.TOKEN)
-        localStorage.removeItem(storageKeys.USER)
-        this.setState({ isLoggedIn: false, storedUser: undefined })
-    }
-
     render() {
+        const { onLogin, onLogout } = this.props
+        const { ethToUsdRate } = this.state
         return (
-            <div className="App">
-                <Router>
-                    <div>
-                        <Navigation
-                            isLoggedIn={this.state.isLoggedIn}
-                            userId={get(this.state, 'storedUser.id', undefined)}
-                        />
-                        <Route exact path="/" component={Home} />
-                        <Route
-                            path="/newest"
-                            render={props => <NewsFeedContainer {...props} ethToUsdRate={this.state.ethToUsdRate} />}
-                        />
-                        <Route path="/signup" component={SignupContainer} />
-                        <Route
-                            path="/login"
-                            render={() => <LoginContainer onLogin={this.onLogin} isLoggedIn={this.state.isLoggedIn} />}
-                        />
-                        <Route
-                            path="/create-post"
-                            render={() => (
-                                <CreateNewsItemContainer
-                                    userId={this.state.storedUser.id}
-                                    isLoggedIn={this.state.isLoggedIn}
+            <AuthContext.Consumer>
+                {({ isLoggedIn, storedUserId }) => (
+                    <div className="App">
+                        <Router>
+                            <div>
+                                <Navigation isLoggedIn={isLoggedIn} userId={storedUserId} />
+                                <Route exact path="/" component={Home} />
+                                <Route
+                                    path="/newest"
+                                    render={props => <NewsFeedContainer {...props} ethToUsdRate={ethToUsdRate} />}
                                 />
-                            )}
-                        />
-                        <Route
-                            path="/news-item-detail"
-                            render={props => <NewsItemDetailView {...props} ethToUsdRate={this.state.ethToUsdRate} />}
-                        />
-                        <Route path="/submissions" component={Submissions} />
-                        <Route path="/logout" render={() => <Logout onLogout={this.onLogout} />} />
+                                <Route path="/signup" component={SignupContainer} />
+                                <Route
+                                    path="/login"
+                                    render={() => <LoginContainer onLogin={onLogin} isLoggedIn={isLoggedIn} />}
+                                />
+                                <Route
+                                    path="/create-post"
+                                    render={() => (
+                                        <CreateNewsItemContainer userId={storedUserId} isLoggedIn={isLoggedIn} />
+                                    )}
+                                />
+                                <Route
+                                    path="/news-item-detail"
+                                    render={props => <NewsItemDetailView {...props} ethToUsdRate={ethToUsdRate} />}
+                                />
+                                <Route path="/submissions" component={Submissions} />
+                                <Route path="/logout" render={() => <Logout onLogout={onLogout} />} />
+                            </div>
+                        </Router>
                     </div>
-                </Router>
-            </div>
+                )}
+            </AuthContext.Consumer>
         )
     }
 }
