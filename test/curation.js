5f47a2dd70c3a6a989378c6055e2fa795005b096
ryosua <hi@ryanyosua.me>
2018-07-14 12:33:52 -0400

    Cleanup.

--- test/curation.js
+++ test/curation.js
@@ -1,30 +1,23 @@
 const Curation = artifacts.require('Curation')
 
+const postId = 'cjj25lgft5e3p0149sjtcmrlo'
+
 contract('Test Curation contract', async accounts => {
     it('Can create a post', async () => {
         const instance = await Curation.deployed()
-
-        const postId = 'cjj25lgft5e3p0149sjtcmrlo'
         const returnedPostid = await instance.createPost.call(postId)
-
         assert.equal(postId, returnedPostid)
     })
 
     it('Can upvote a post', async () => {
         const instance = await Curation.deployed()
-
-        const postId = 'cjj25lgft5e3p0149sjtcmrlo'
         const returnedPostid = await instance.upVotePost.call(postId)
-
         assert.equal(postId, returnedPostid)
     })
 
     it('Can downvote a post', async () => {
         const instance = await Curation.deployed()
-
-        const postId = 'cjj25lgft5e3p0149sjtcmrlo'
         const returnedPostid = await instance.downVotePost.call(postId)
-
         assert.equal(postId, returnedPostid)
     })
 })
