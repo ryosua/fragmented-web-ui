d2562340351b603ebba81219e8068d3434e2bb82
ryosua <hi@ryanyosua.me>
2018-09-15 19:44:46 -0400

    Modified the upvote test (contract state is limited to each it block)

--- test/curation.js
+++ test/curation.js
@@ -6,18 +6,22 @@ contract('Test Curation contract', async accounts => {
     it('Can create a post', async () => {
         const instance = await Curation.deployed()
         const returnedPostid = await instance.createPost.call(postId)
+        console.log(`create: ${JSON.stringify(returnedPostid)}`)
         assert.equal(postId, returnedPostid)
     })
 
     it('Can upvote a post', async () => {
         const instance = await Curation.deployed()
+        const createdPostId = await instance.createPost.call(postId)
         const returnedPostid = await instance.upVotePost.call(postId)
-        assert.equal(postId, returnedPostid)
+        console.log(`upvote: ${JSON.stringify(returnedPostid)}`)
+        assert.equal(createdPostId, returnedPostid)
     })
 
     it('Can downvote a post', async () => {
         const instance = await Curation.deployed()
         const returnedPostid = await instance.downVotePost.call(postId)
+        console.log(`downvote: ${JSON.stringify(returnedPostid)}`)
         assert.equal(postId, returnedPostid)
     })
 })
